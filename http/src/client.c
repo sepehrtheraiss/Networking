549f94fed571615bfc5ccd0134b8525d31991493
Sepehr Raissian <sepehrtheraiss@gmail.com>
2018-03-10 18:39:28 -0800

    damit c

--- http/src/client.c
+++ http/src/client.c
@@ -11,15 +11,7 @@
 #include <arpa/inet.h>
 int main()
 {
-const char msg[] ="GET http://www.webscantest.com/ HTTP/1.1\r\n
-Host: www.webscantest.com\r\n
-User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:58.0) Gecko/20100101 Firefox/58.0\r\n
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n
-Accept-Language: en-US,en;q=0.5\r\n
-Accept-Encoding: gzip, deflate\r\n
-Cookie: TEST_SESSIONID=9o55ff742rp5gt78276vclmqu1; NB_SRVID=srv140717\r\n
-Connection: keep-alive\r\n
-Upgrade-Insecure-Requests: 1\r\n";
+const char msg[] ="GET http://www.webscantest.com/ HTTP/1.1\r\nHost: www.webscantest.com\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:58.0) Gecko/20100101 Firefox/58.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nCookie: TEST_SESSIONID=9o55ff742rp5gt78276vclmqu1; NB_SRVID=srv140717\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\n";
 
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   struct sockaddr_in serv_addr;
